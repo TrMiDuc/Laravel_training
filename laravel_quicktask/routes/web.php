57505f2ec5e8402ae3429e217d2c2a4f23c42c0c
TrMiDuc <ductruong9922@gmail.com>
2025-07-29 14:35:51 +0700

    quicktask5
    quicktask6

--- laravel_quicktask/routes/web.php
+++ laravel_quicktask/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Middleware\CheckSuperAdmin;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class,'create'])->name('tasks.create');
+Route::get('/tasks/edit', [TaskController::class,'edit'])->name('tasks.edit');
+Route::get('/tasks/{task}', [TaskController::class,'show'])->name('tasks.show');
+Route::post('/tasks', [TaskController::class,'store'])->name('tasks.store');
+Route::put('/tasks/{task}', [TaskController::class,'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class,'destroy'])->name('tasks.destroy');
+
+Route::middleware(CheckSuperAdmin::class)->group(function () 
+{
+    Route::resource('/users', UserController::class);
 });
